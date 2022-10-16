831c3d4139c5612413e5aa3d597e6db088b82547
Suralal001 <suralal.s@neoito.com>
2022-10-17 00:37:56 +0530

    changes print command

--- binarySearch.js
+++ binarySearch.js
@@ -1,4 +1,4 @@
-/* Returns either the index of the location in the array,
+/* Returns either the index of the location in the array ,
   or -1 if the array did not contain the targetValue */
   var doSearch = function(array, targetValue) {
     var min = 0;
@@ -8,7 +8,7 @@
           guess = Math.floor((min + max)/2); 
           if(array[guess] === targetValue){
               
-              println("Found it: ");
+              println("Found it ding dong: ");
               return guess;
           }
           else if(array[guess] < targetValue){
